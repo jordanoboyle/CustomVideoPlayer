b8538695e8c085802dfc673fbd1bfe721c2b5a4e
jordanoboyle <jmcmcode@gmail.com>
2024-08-21 22:39:27 -0600

    define functions with basic return to create checklist

--- script.js
+++ script.js
@@ -5,7 +5,30 @@ const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
+//Play and Pause Video
+function toggleVideoStatus() {
+  return true;
+}
 
+//Update Play Pause Icon
+function updatePlayIcon() {
+  return true;
+}
+
+//Update progress and timestamp
+function updateProgress() {
+  return true;
+}
+
+//Set Video time to progress
+function setVideoProgress() {
+  return true;  
+}
+
+//Stop the Video
+function stopVideo() {
+  return true;
+}
 
 //Event Listeners (This can be an outline of the functions that need to be written)
 video.addEventListener('click', toggleVideoStatus);
